e10441126aed0dc8f2d8967cb6547705ef7a746a
Aegas <support@aegas.io>
2024-01-03 10:31:50 +0200

    feat(core): impl EmisionManager, save to hold
    Saving recent changes by switching to another task

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,17 @@ import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.19',
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+      evmVersion: 'paris',
+    },
+  },
 };
+
 
 export default config;
