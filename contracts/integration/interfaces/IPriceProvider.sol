b3ec4b9cf1dacfa86cf8eea4be5d7716564ce773
Aegas <support@aegas.io>
2024-05-16 01:30:12 +0300

    * fix typos

--- contracts/integration/interfaces/IPriceProvider.sol
+++ contracts/integration/interfaces/IPriceProvider.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.8.0;
 
 /**
  * @title IPriceProvider Interface
- * @dev Interface for the price provider that defines a function to retrieve the USD to FNX price.
+ * @dev Interface for the price provider that defines a function to retrieve the USD to TOKEN price.
  */
 interface IPriceProvider {
     /**
