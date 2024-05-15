b5c82ce99eac27b84be05bc36b4d5ed076106c3b
Aegas <support@aegas.io>
2024-05-16 01:04:00 +0300

    remove blast support

--- contracts/integration/interfaces/IBlastPoints.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0;
-
-interface IBlastPoints {
-    function configurePointsOperator(address operator) external;
-
-    function configurePointsOperatorOnBehalf(address contractAddress, address operator) external;
-}
