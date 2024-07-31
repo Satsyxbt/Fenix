c54b55185e0a5944e640843a53d6e79337f743da
Aegas <support@aegas.io>
2024-07-31 18:07:25 +0300

    upgrade FenixMerklAidrop on blastSepolia

---
+++ deploy/blastSepolia/013_UpgradeFenixMerklAidrop-upgrade.ts
@@ -0,0 +1,32 @@
+import {
+  AliasDeployedContracts,
+  deployNewImplementationAndUpgradeProxy,
+  getBlastGovernorAddress,
+  getDeployedContractsAddressList,
+  getProxyAdminAddress,
+} from '../../utils/Deploy';
+import { ethers } from 'hardhat';
+import { InstanceName } from '../../utils/Names';
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  const BlastGovernor = await getBlastGovernorAddress();
+  const DeployedContracts = await getDeployedContractsAddressList();
+
+  await deployNewImplementationAndUpgradeProxy({
+    implementationName: InstanceName.VeFnxSplitMerklAidropUpgradeable,
+    deployer: deployer,
+    implementationConstructorArguments: [BlastGovernor],
+    implementationSaveAlias: AliasDeployedContracts.VeFnxDistributorUpgradeable_Implementation,
+    proxyAddress: DeployedContracts[AliasDeployedContracts.VeFnxDistributorUpgradeable_Proxy],
+    proxyAdmin: await getProxyAdminAddress(),
+    verify: true,
+  });
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
