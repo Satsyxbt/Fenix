9fe331d054e038f80d35fad5904a6c8685139801
Aegas <support@aegas.io>
2024-11-27 12:10:45 +0200

    deploy aggregate claim to lock in testnet

---
+++ deploy/blastSepolia/051-Upgrade Voter to last version.ts
@@ -0,0 +1,33 @@
+import { ethers } from 'hardhat';
+import {
+  AliasDeployedContracts,
+  deployNewImplementationAndUpgradeProxy,
+  getBlastGovernorAddress,
+  getDeployedContractsAddressList,
+  getProxyAdminAddress,
+} from '../../utils/Deploy';
+import { InstanceName } from '../../utils/Names';
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  const DeployedContracts = await getDeployedContractsAddressList();
+  const ProxyAdmin = await getProxyAdminAddress();
+  const BlastGovernor = await getBlastGovernorAddress();
+
+  await deployNewImplementationAndUpgradeProxy({
+    implementationName: InstanceName.VoterUpgradeable,
+    deployer: deployer,
+    implementationConstructorArguments: [BlastGovernor],
+    implementationSaveAlias: AliasDeployedContracts.VoterUpgradeable_Implementation,
+    proxyAddress: DeployedContracts[AliasDeployedContracts.VoterUpgradeable_Proxy],
+    proxyAdmin: await getProxyAdminAddress(),
+    verify: true,
+  });
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
